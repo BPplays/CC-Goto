8f9e3593d3968b85d63c90e66c58eaeeb131998a
BPplays <BruceWplays@gmail.com>
2023-02-21 08:06:23 -0800

    changed to search for modems

--- Goto.lua
+++ Goto.lua
@@ -4,10 +4,9 @@ function gotoapi(gx, gy, gz, enddirlet)
     local r = 1
     tru = 1
     local tturned = 0
-    local modems = { peripheral.find("modem", function(name, modem)
-      return modem.isWireless() -- Check this modem is wireless.
-    end) }
-    rednet.open(modems[1])
+    -- local modems = 
+    peripheral.find("modem", rednet.open)
+    -- rednet.open(modems[1])
     local cx,cy,cz = gps.locate(2,false)
     if (gx and gy and gz) == nil then
       print("Incorrect usage of Function.")
